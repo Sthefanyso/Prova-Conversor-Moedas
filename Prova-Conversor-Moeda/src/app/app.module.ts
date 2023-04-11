b531d8ad5cdc14983586f3a35487e95b8b43a323
Sthefanyso <sthefany.silva3@fatec.sp.gov.br>
2023-04-11 08:49:25 -0300

    Primeiro Commit

---
+++ Prova-Conversor-Moeda/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ConversorComponent } from './conversor/conversor.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ConversorComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
